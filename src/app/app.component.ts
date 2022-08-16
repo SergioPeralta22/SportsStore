caa81640b28d114efd9ffa3c89b161f2cd2bd278
Sergio Peralta <seperalta88@gmail.com>
2022-08-16 15:27:56 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+/**
+ * no hay codigo en el app.component.ts porque el componente principal solo existe para manejar el contenido mostrado al usuario
+ */
+
+@Component({
+  selector: 'app',
+  template: `<div class="bg-success p-2  text-center text-white">
+    This is SportsStore
+  </div>`,
+})
+export class AppComponent {}
